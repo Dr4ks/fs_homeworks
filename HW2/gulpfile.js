06c191a4e4b06880b70ba94ebb5ca843c7549e91
Dr4ks <sahib.humbatzada.std@bhos.edu.az>
2023-06-12 15:54:04 +0400

    HW2

--- HW2/gulpfile.js
+++ HW2/gulpfile.js
@@ -1,73 +1,68 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-const clean = require('gulp-clean')
-const browserSync = require('browser-sync').create();
-const uglify=require('gulp-uglify');
-const sass=require('gulp-sass')(require('sass'))
-const cleancss=require('gulp-clean-css');
-const autoprefixer=require('gulp-autoprefixer');
-const imagemin=require('gulp-imagemin');
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var clean = require('gulp-clean')
+var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass')(require('sass'));
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
 
 
-gulp.task("compileSCSS",function(){
-    return gulp.src("src/scss/*.scss")
-    .pipe(sass().on('error',sass.logError)) //minifying and compiling
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 2 versions', 'Firefox >= 60', 'Chrome >= 62', 'Safari >= 11', 'iOS >= 11', 'Edge >= 16'],
-        cascade: false
-    }))   
-    .pipe(cleancss({compatibility:'ie8'}))
-    .pipe(concat("styles.min.css")) //concating
-    .pipe(gulp.dest('dist/styles'))
-});
-
-gulp.task("compileJS",function(){
-    return gulp.src("src/js/*.js")
-    .pipe(uglify())  //minifying
-    .pipe(concat("scripts.min.js")) //concating
-    .pipe(gulp.dest('dist/scripts'))
-});
 
 
-//this is dev task
-gulp.task("dev",function(){
-    browserSync.init({
-        server:{baseDir:'./'},index:'index.html'
-    })
-    gulp.watch("/src/scss/*.scss",gulp.series("compileSCSS")).on('change',browserSync.reload);
-    gulp.watch("/src/js/*.js",gulp.series("compileJS")).on('change',browserSync.reload);
-    gulp.watch('src/img/*', gulp.series(['optimg'])).on('change', browserSync.reload);
-})
 
-//cleaning dist folder
-gulp.task('clean', function () {
+gulp.task('clear', () => {
     return gulp.src('dist/*', { read: false }).pipe(clean())
+})
+
+gulp.task('minifyCss', () => {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError)) 
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions', 'Firefox >= 60', 'Chrome >= 62', 'Safari >= 11', 'iOS >= 11', 'Edge >= 16'],
+            cascade: false
+        })) 
+        .pipe(cleanCSS({ compatibility: 'ie8' })) 
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minifyJs', () => {
+    return gulp.src('src/js/*.js')
+        .pipe(uglify()) 
+
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest('dist'))
+
+})
+
+gulp.task('optimizeImages', () => {
+    return gulp.src("src/img/*")
+        .pipe(imagemin()) 
+        .pipe(gulp.dest('dist/images'))
+})
+
+
+//build task is here
+gulp.task('build', gulp.series(['clear', 'minifyCss', 'minifyJs', 'optimizeImages']))
+
+
+
+gulp.task('startServer', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        index: 'index.html'
+    })
+    gulp.watch('src/scss/**/*.scss', gulp.series(['minifyCss'])).on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', gulp.series(['minifyJs'])).on('change', browserSync.reload);
+    gulp.watch('src/img/*', gulp.series(['optimizeImages'])).on('change', browserSync.reload);
 });
 
 
-
-//remove unused css code
-gulp.task('rmuncss',function(){
-    return gulp.src('src/img/**/*')
-    .pipe(uncss({
-        html:['*.html']
-    }))
-    .pipe(gulp.dest('dist/styles'))
-})
-
-//optimize image
-gulp.task('optimg', function () {
-    return gulp.src('src/img/**/*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('dist/images'))
-  });
-
-
-//this is build task
-
-gulp.task('build',gulp.series(['compileSCSS','compileJS','clean','rmuncss','optimg']))
-
-
-
+//dev task is here
+gulp.task('dev', gulp.series(['build', 'startServer']))
 
 
